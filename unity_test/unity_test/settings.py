7f36e6282e1b85b2d4e6dbbb57c48ee24b8fadc1
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-10-08 00:28:26 +0600

    template added

--- unity_test/unity_test/settings.py
+++ unity_test/unity_test/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = BASE_DIR / 'templates'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'unity_test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
