8113ef794b5727f5bd15c22738f31ef6b6400c1b
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-10-08 00:28:02 +0600

    validation updated

--- unity_test/unity/serializers.py
+++ unity_test/unity/serializers.py
@@ -16,12 +16,5 @@ class EmailSubscriptionCRUDSerializer(serializers.ModelSerializer):
         read_only_fields = ("created_at", "updated_at")
         required = ("email",)
 
-    def validate_email(self, value):
-        try:
-            EmailSubscription.objects.get(email=value)
-            return value
-        except:
-            raise serializers.ValidationError("Email not found")
-
     def create(self, validated_data):
         return super().create(validated_data)
